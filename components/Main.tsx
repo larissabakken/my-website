c1db921f16a0b39152518b2eb5833378db0eded1
Larissa Ramos <devlarissasr@gmail.com>
2023-01-11 15:31:40 +0100

    main content

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 export const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1024px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="py-2 uppercase text-sm tracking-widest text-gray-600">
+            Let's build something great together.
+          </p>
+          <h1 className="py-2 text-gray-800">
+            Hi, I'm <span className="text-blue-500">Larissa</span>
+          </h1>
+          <h1 className="py-2 text-gray-800">
+            A <span className="text-blue-500">FullStack</span> Developer
+          </h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            non dolorem repudiandae dolore cum, hic iste sequi animi totam
+            magnam, id nobis. Veniam, id culpa est excepturi at vero
+            repudiandae.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Navbar } from '../components/Navbar';
+import { Main } from '../components/Main';
 import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <Main />
     </div>
   );
 }
