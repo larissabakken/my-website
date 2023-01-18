2a1068c10bc20f342726d782017f56ed8c4e91ab
Larissa Ramos <devlarissasr@gmail.com>
2023-01-18 12:20:45 +0100

    about fixing photos

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const picture =
-  'https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80';
+const picture ='/assets/profile.jpeg';
 
 export const About = () => {
   return (
